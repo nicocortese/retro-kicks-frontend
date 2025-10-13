c59fe325b9c2a3f36ba0fb77437e5147684c99cc
nicocortese <nicolascortese2004@gmail.com>
2025-10-13 17:58:39 -0300

    cambios en imagenes

--- src/components/CheckoutContainer.js
+++ src/components/CheckoutContainer.js
@@ -4,18 +4,27 @@ import { useShopContext } from "@/contexts/ShopContext";
 const CheckoutContainer = () => {
   const { cart } = useShopContext();
 
-  console.log(cart); // 👈 Agregalo acá
-
+  const handlePlaceOrder = () => {
+    
+  }
 
   return (
-    <div>
+    <section className="max-w-[1200px] my-0 mx-auto pt-20 py-3">
+    <div className="grid grid-cols-12">
+      <div className="col-span-6 flex justify-center items-center">
+        FORM USER
+        </div>
+        <div className="col-span-6 flex flex-col justify-center items-start">
+          <h2>PEDIDO</h2>
       {cart.map((product) => (
-        <div key={product._id}>
-          <h3 className="pt-20">{product.name}</h3>
+        <div key={product._id} className="border-8 border-solid border-red-500 p-2 w-100">
+          <h3 className="pt-20 ">{product.name}</h3>
           <p className="">Cantidad: {product.qty}</p>
         </div>
       ))}
     </div>
+    </div>
+    </section>
   );
 };
 
